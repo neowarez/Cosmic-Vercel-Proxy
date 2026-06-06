7b8470a95e338190857cb855c8355a8126f206d1
neowarez <nathan.hhorton@gmail.com>
2026-06-06 12:22:46 +0200

    Refactor handler to use BaseHTTPRequestHandler

--- api/exfil.py
+++ api/exfil.py
@@ -1,7 +1,12 @@
+from http.server import BaseHTTPRequestHandler
 import json
 
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "OK"})
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps({"message": "OK"}).encode()
+        )
