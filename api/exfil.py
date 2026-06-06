c647046b7bf7cf5ce22bcdadfad860c97194185c
neowarez <nathan.hhorton@gmail.com>
2026-06-06 12:32:51 +0200

    Update exfil.py

--- api/exfil.py
+++ api/exfil.py
@@ -3,5 +3,6 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(b"OK")
+        self.wfile.write(b'{"status":"ok"}')
