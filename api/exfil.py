c7d3ce9df09183f9c28d5b97db9e6c4cebeda115
neowarez <nathan.hhorton@gmail.com>
2026-06-06 12:31:27 +0200

    Update exfil.py

--- api/exfil.py
+++ api/exfil.py
@@ -3,6 +3,5 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from Vercel")
+        self.wfile.write(b"OK")
